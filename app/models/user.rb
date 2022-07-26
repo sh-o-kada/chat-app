8dd712457f46b9ffa961ec56a137f29b7843437c
sh-o-kada <shokada.25y@gmail.com>
2022-07-26 14:59:51 +0900

    shuuseinoshuusei

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :email, presence: true
 end
